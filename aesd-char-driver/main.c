527d3727658ab0adebcdfd6faa6e888bf528355e
Sara Edwards <sara.kay.edwards@gmail.com>
2026-05-14 23:35:40 -0700

    restored much of aesd_read()

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -61,23 +61,15 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
     size_t retOffset;
     int bytesRem;
     int bytesNotCopied;
-    size_t mycount;
-    loff_t myf_pos;
     
-    //if ((bytesNotCopied = copy_from_user(&mycount, &count, sizeof(size_t))) != 0) {return 0;}
-
-    //if ((bytesNotCopied = copy_from_user(&myf_pos, f_pos, sizeof(loff_t))) != 0) {return 0;}
-    mycount = count;
-    myf_pos = *f_pos;
-    
-    PDEBUG("read %zu bytes with offset %lld",mycount,myf_pos);
+    PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
 
 
     // get the semaphore before we write to the buffer
     if ((retval = mutex_lock_interruptible(&aesd_mutex)) != 0) return retval;
 
     if ((retEntry = aesd_circular_buffer_find_entry_offset_for_fpos(&(dev->buff),
-            (size_t)(myf_pos), &retOffset)) == NULL) {
+            (size_t)(*f_pos), &retOffset)) == NULL) {
         mutex_unlock(&aesd_mutex);
         return 0;
     }
@@ -88,9 +80,7 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
     mutex_unlock(&aesd_mutex);
     
     // update f_pos
-    myf_pos = myf_pos + bytesRem - bytesNotCopied;
-    //if ((bytesNotCopied = copy_to_user(f_pos, &myf_pos, sizeof(loff_t))) != 0) {return 0;}
-    *f_pos = myf_pos;
+    *f_pos = *f_pos + bytesRem - bytesNotCopied;
     
     // return the number of bytes we read
     retval = bytesRem - bytesNotCopied;
@@ -241,10 +231,10 @@ int aesd_adjust_file_offset(struct file* filp, unsigned int write_cmd, unsigned 
     return 0;  // success
 }
 
-long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+long int aesd_ioctl(struct file* filp, unsigned int cmd, long unsigned int arg) {
     
     struct aesd_seekto seekto;
-    long retval = 0;
+    long int retval = 0;
 
     switch (cmd) {
     
@@ -253,7 +243,8 @@ long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
                 retval = EFAULT;
             }
             else {
-                retval = aesd_adjust_file_offset(filp, seekto.write_cmd, seekto.write_cmd_offset);
+                retval = aesd_adjust_file_offset(filp, seekto.write_cmd, 
+                                                 seekto.write_cmd_offset);
             }
             PDEBUG("handling the AESDCHAR_IOCSEEKTO command with %d, %d", 
                    seekto.write_cmd, seekto.write_cmd_offset);
